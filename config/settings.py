a9967b8a1baf48f74eb0973774258e94f6e49173
Gaurav <gauravsomani52750@gmail.com>
2022-12-27 10:58:41 +0530

    created our custom user model

--- config/settings.py
+++ config/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# *Tell Django to use our custom user model inplace of built-in User Model:
+AUTH_USER_MODEL = 'accounts.CustomUser'
