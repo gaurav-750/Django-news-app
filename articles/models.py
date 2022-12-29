651ea4f828d63deef5577bddfcd5fb5e8cd77bcb
Gaurav <gauravsomani52750@gmail.com>
2022-12-29 09:58:22 +0530

    added articles pages at /articles

--- articles/models.py
+++ articles/models.py
@@ -8,7 +8,7 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    autor = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
